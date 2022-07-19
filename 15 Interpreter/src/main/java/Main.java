74f770e0fdd292142e3f4d208feb6f9f329924b5
Лісовий Віктор Юрійович <victor.lissoviy.93@gmail.com>
2022-07-19 07:18:14 +0300

    modify gitignore

--- 15 Interpreter/src/main/java/Main.java
+++ 15 Interpreter/src/main/java/Main.java
@@ -18,6 +18,7 @@ public final class Main {
    */
   public static void main(final String[] args) {
     String stringExpression = "12578-278+33669";
+//    String stringExpression = "2+(2-2)+2";
     Context context = new Context();
     Expression expression = context.evaluateExpression(stringExpression);
     System.out.println(stringExpression + " = " + expression.interpret());
